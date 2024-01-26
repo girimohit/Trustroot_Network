c08a02a916f3342d2defa4e685ddcb3926810d99
[mohit] <117682356+girimohit@users.noreply.github.com>
2024-01-27 02:09:59 +0530

    Project Structure

---
+++ authentication/urls.py
@@ -0,0 +1,8 @@
+# myapp/urls.py
+
+from django.urls import path
+from authentication import views
+
+urlpatterns = [
+    # Define more URL patterns as needed
+]
---
+++ baseapp/urls.py
@@ -0,0 +1,8 @@
+# myapp/urls.py
+
+from django.urls import path
+from baseapp import views
+
+urlpatterns = [
+    # Define more URL patterns as needed
+]
