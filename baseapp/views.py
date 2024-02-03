5d0239420e3df4c9198a65b22cc83de2dfaa971a
Mohit Giri <117682356+girimohit@users.noreply.github.com>
2024-02-03 16:59:02 +0530

    Update views.py

--- baseapp/views.py
+++ baseapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
 
 # Create your views here.
+def home(request):
+    return render(request, "index.html")
+
+
+def about(request):
+    return render(request, "about_us.html")
