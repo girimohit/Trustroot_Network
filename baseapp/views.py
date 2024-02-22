cb6eab320feeb338ba0449012a13b73f9f569d70
Mohit Giri <mohitgiri1103@gmail.com>
2024-02-23 00:41:56 +0530

    Update frontend

--- baseapp/views.py
+++ baseapp/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpRequest, HttpResponse
 
 
 # Create your views here.
 def home(request):
-    return render(request, "index.html")
+    form = UserCreationForm()
+    # context = {'signform' : form}
+    return render(request, 'index.html')
+
 
 
 def about(request):
